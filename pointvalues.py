019e71be530864d11f78c9ecf8f744c748341865
Paul Cutler <paul.r.cutler@gmail.com>
2016-10-07 10:05:12 -0500

    Add pointvalues.py with point values for each possible category

---
+++ pointvalues.py
@@ -0,0 +1,94 @@
+#Point values for each category
+
+#Division Points
+
+afc_east_first = 50
+afc_east_second = 30
+afc_east_last = 20
+
+afc_north_first = 50
+afc_north_second = 30
+afc_north_last = 20
+
+afc_south_first = 50
+afc_south_second = 30
+afc_south_last = 20
+
+afc_west_first = 50
+afc_west_second = 30
+afc_west_last = 20
+
+nfc_east_first = 50
+nfc_east_second = 30
+nfc_east_last = 20
+
+nfc_north_first = 50
+nfc_north_second = 30
+nfc_north_last = 20
+
+nfc_south_first = 50
+nfc_south_second = 30
+nfc_south_last = 20
+
+nfc_west_first = 50
+nfc_west_second = 30
+nfc_west_last = 20
+
+#Playoff Wildcards
+
+afc_wildcard1 = 25
+afc_wildcard2 = 25
+nfc_wildcard1 = 25
+nfc_wildcard2 = 25
+
+#Individual Stats
+
+afc_rushing_first = 30
+afc_rushing_second = 20
+afc_rushing_third = 10
+
+afc_passing_first = 30
+afc_passing_second = 20
+afc_passing_third = 10
+
+afc_receiving_first = 30
+afc_receiving_second = 20
+afc_receiving_third = 10
+
+afc_sacks_first = 30
+afc_sacks_second = 20
+afc_sacks_third = 10
+
+afc_int_first = 30
+afc_int_second = 20
+afc_int_third = 10
+
+nfc_rushing_first = 30
+nfc_rushing_second = 20
+nfc_rushing_third = 10
+
+nfc_passing_first = 30
+nfc_passing_second = 20
+nfc_passing_third = 10
+
+nfc_receiving_first = 30
+nfc_receiving_second = 20
+nfc_receiving_third = 10
+
+nfc_sacks_first = 30
+nfc_sacks_second = 20
+nfc_sacks_third = 10
+
+nfc_int_first = 30
+nfc_int_second = 20
+nfc_int_third = 10
+
+
+#Conference Team with most Points For
+
+afc_pf = 20
+nfc_pf = 20
+
+#Tiebreaker
+
+specialteams_td = 1000
