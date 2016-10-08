709d6c530296fe9e0b03ad5ed28facd7c69b93fa
Paul Cutler <paul.r.cutler@gmail.com>
2016-10-08 16:29:08 -0500

    Create JSON importer from raw JSON file including web headers and pretty print all of the stat categories for one file

--- importjson.py
+++ importjson.py
@@ -2,10 +2,14 @@ import json
 import pprint
 import os
 
+#Open the JSON file that includes headers
 
-with open('json/test.json') as json_data:
-    d = json.load(json_data)
-#    print(d)
-#    pprint.pprint(d)
-    for stat_categories in d["divisionteamstandings"]["division"][0]["teamentry"][0]["stats"]:
-        pprint.pprint(stat_categories)
+with open('json/20160927-division-team-standings.json') as file:
+    alltext = file.readlines()  #Put each line into a list
+
+for lines in alltext:
+    if lines.startswith('{'):
+        rawdata = lines
+        data = json.loads(rawdata)
+        for stat_categories in data["divisionteamstandings"]["division"][0]["teamentry"][0]["stats"]:
+            pprint.pprint(stat_categories)
