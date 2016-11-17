a186d6fb5bebf3adf0b40870693191c1762fd080
Paul Cutler <paul.r.cutler@gmail.com>
2016-11-17 09:20:52 -0600

    Revert "Add requests_test.py - can successfully use secret.py with your MSF login/pw to Base64 authenticate the feed and get a 200 response code from MSF"
    This reverts commit 4574225c58e5dac16dd82370af05f17bf4dc33d1.

--- requests_test.py
+++
@@ -1,9 +0,0 @@
-import requests
-from requests.auth import HTTPBasicAuth
-import secret
-
-
-response = requests.get('https://www.mysportsfeeds.com/api/feed/pull/nfl/2016-2017-regular/division_team_standings.json',
-             auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
-
-print(response.status_code)
