a2baf87dca84f4d581d78ea7fa8c194dc2283c95
Paul Cutler <paul.r.cutler@gmail.com>
2016-11-17 20:37:36 -0600

    Fixed requests to get response 200 when passing parameters to the API feed (yay!)

--- requests_test.py
+++ requests_test.py
@@ -2,8 +2,16 @@ import requests
 from requests.auth import HTTPBasicAuth
 import secret
 
+parameters = 'teamstats'
 
-response = requests.get('https://www.mysportsfeeds.com/api/feed/pull/nfl/2016-2017-regular/division_team_standings.json',
-             auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+response = requests.get('https://www.mysportsfeeds.com/api/feed/pull/nfl/2016-2017-regular/playoff_team_standings.json?teamstats',
+                        auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
 
 print(response.status_code)
+
+# Make a get request with the parameters.
+#response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+
+#https://www.mysportsfeeds.com/api/feed/pull/nfl/{2016-2017}/playoff_team_standings.json
+#{format}?teamstats={team-stats}
+#https://www.mysportsfeeds.com/api/feed/pull/nfl/2016-2017/playoff_team_standings.json
