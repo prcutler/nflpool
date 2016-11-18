c162514291428f26dc78d08393455ff33fe12f12
Paul Cutler <paul.r.cutler@gmail.com>
2016-11-17 20:38:21 -0600

    Clean up file and remove notes now that parameters in API feed are working

--- requests_test.py
+++ requests_test.py
@@ -8,10 +8,3 @@ response = requests.get('https://www.mysportsfeeds.com/api/feed/pull/nfl/2016-20
                         auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
 
 print(response.status_code)
-
-# Make a get request with the parameters.
-#response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
-
-#https://www.mysportsfeeds.com/api/feed/pull/nfl/{2016-2017}/playoff_team_standings.json
-#{format}?teamstats={team-stats}
-#https://www.mysportsfeeds.com/api/feed/pull/nfl/2016-2017/playoff_team_standings.json
