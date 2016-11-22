2dfaf6fbd7428d41c47e7966bf089285cdcc51d1
Paul Cutler <paul.r.cutler@gmail.com>
2016-11-21 21:18:47 -0600

    Update weekly_player_stats.py - still horribly broken but added a TODO that might help me remember how to fix it

--- weekly_player_stats.py
+++ weekly_player_stats.py
@@ -21,12 +21,17 @@ for lines in alltext:
             player_firstname = data['cumulativeplayerstats']['playerstatsentry'][v]['player']['FirstName']
             player_lastname = data['cumulativeplayerstats']['playerstatsentry'][v]['player']['LastName']
             player_position = data['cumulativeplayerstats']['playerstatsentry'][v]['player']['Position']
+            print((player_id), (player_firstname), (player_lastname), rushing_yards)
             v += 1
-            print((player_id), (player_firstname), (player_lastname), rushing_yards)
 
-            rushing_yards = data['cumulativeplayerstats']['playerstatsentry'][1]['stats']['RushYards']['#text']
-            player_id = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['ID']
-            player_firstname = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['FirstName']
-            player_lastname = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['LastName']
-            player_position = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['Position']
-            print((player_id), (player_firstname), (player_lastname), rushing_yards)
+            #            rushing_yards = data['cumulativeplayerstats']['playerstatsentry'][6]['stats']['RushYards']['#text']
+            #            player_id = data['cumulativeplayerstats']['playerstatsentry'][6]['player']['ID']
+            #            player_firstname = data['cumulativeplayerstats']['playerstatsentry'][6]['player']['FirstName']
+            #            player_lastname = data['cumulativeplayerstats']['playerstatsentry'][6]['player']['LastName']
+            #            player_position = data['cumulativeplayerstats']['playerstatsentry'][6]['player']['Position']
+            #            print((player_id), (player_firstname), (player_lastname), rushing_yards)
+
+            rushing_yards = data['cumulativeplayerstats']['playerstatsentry'][6]['stats']
+            print(rushing_yards)
+
+            # TODO: Either do query based on position or write an if / elif statement or try / except if Key Error returned
