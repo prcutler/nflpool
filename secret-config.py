9eb8ba8496855bd851274a6c7bcdce0ee0ce5e6c
Paul Cutler <paul.r.cutler@gmail.com>
2016-11-23 15:34:20 -0600

    Add secret-config.py for users to add their MySportsFeed username and password

---
+++ secret-config.py
@@ -0,0 +1,7 @@
+
+
+# You will need an account from MySportsFeed to access their API.  They offer free access to developers
+# Edit below with your credentials and then save as secret.py
+
+msf_username = YOURUSERNAME
+msf_pw = YOURPASSWORD
