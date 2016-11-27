864ba261ee725792a89364a38f3d0205f41415b8
Paul Cutler <paul.r.cutler@gmail.com>
2016-11-27 08:43:54 -0600

    Clean up weekly_player_stats.py file and remove notes and code from testing

--- weekly_player_stats.py
+++ weekly_player_stats.py
@@ -4,11 +4,6 @@ import json
 with open('json/20161018-cumulative-player-stats.json') as file:
     alltext = file.readlines()  # Put each line into a list
 
-v = 0  # Rushing
-w = 0  # Receiving
-x = 0  # Passing
-y = 0  # Sacks
-z = 0  # Interceptions
 
 for lines in alltext:
     if lines.startswith('{'):
@@ -16,18 +11,6 @@ for lines in alltext:
         data = json.loads(rawdata)
 
         player_list = data['cumulativeplayerstats']['playerstatsentry']
-
-        # try:
-        #    for rushing in player_list:
-        #        rushing_yards = data['cumulativeplayerstats']['playerstatsentry'][v]['stats']['RushYards']['#text']
-        #        player_id = data['cumulativeplayerstats']['playerstatsentry'][v]['player']['ID']
-        #        player_firstname = data['cumulativeplayerstats']['playerstatsentry'][v]['player']['FirstName']
-        #        player_lastname = data['cumulativeplayerstats']['playerstatsentry'][v]['player']['LastName']
-        #        player_position = data['cumulativeplayerstats']['playerstatsentry'][v]['player']['Position']
-        #        print((player_id), (player_firstname), (player_lastname), rushing_yards)
-        # except KeyError:
-
-
 
         for rushing in player_list:
             try:
@@ -40,17 +23,3 @@ for lines in alltext:
 
             except KeyError:
                 continue
-
-
-
-                #        rushing_yards = data['cumulativeplayerstats']['playerstatsentry'][1]['stats']['RushYards']['#text']
-                #        player_id = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['ID']
-                #        player_firstname = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['FirstName']
-                #        player_lastname = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['LastName']
-                #        player_position = data['cumulativeplayerstats']['playerstatsentry'][1]['player']['Position']
-                #        print((player_id), (player_firstname), (player_lastname), rushing_yards)
-
-                #            rushing_yards = data['cumulativeplayerstats']['playerstatsentry'][6]['stats']
-                #            print(rushing_yards)
-
-                # TODO: Either do query based on position or write an if / elif statement or try / except if Key Error returned
