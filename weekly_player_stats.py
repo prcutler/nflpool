75c508ed04f705b364b3a9ea7adf1af655ee9701
Paul Cutler <paul.r.cutler@gmail.com>
2016-11-27 09:07:38 -0600

    Attempt to make weekly_player_stats more Pythonic - add first function to get all individual player stats

--- weekly_player_stats.py
+++ weekly_player_stats.py
@@ -1,5 +1,31 @@
 import json
 
+
+def main():
+    stats()
+
+
+def individual_stats():
+    for stats in player_list:
+        try:
+            rushing_yards = stats['stats']['RushYards']['#text']
+            passing_yards = stats['stats']['PassYards']['#text']
+            receiving_yards = stats['stats']['RecYards']['#text']
+            interceptions = stats['stats']['Interceptions']['#text']
+            sacks = stats['stats']['Sacks']['#text']
+            player_id = stats['player']['ID']
+            player_firstname = stats['player']['FirstName']
+            player_lastname = stats['player']['LastName']
+            player_position = stats['player']['Position']
+        #            print((player_id), (player_firstname), (player_lastname), rushing_yards)
+
+        # TODO: Add SQL code to add data to database for each statistical category
+
+        except KeyError:
+            continue
+
+
+
 # Change the name of the file to open to match the query below:
 with open('json/20161018-cumulative-player-stats.json') as file:
     alltext = file.readlines()  # Put each line into a list
@@ -23,3 +49,6 @@ for lines in alltext:
 
             except KeyError:
                 continue
+
+if __name__ == '__main__':
+    main()
