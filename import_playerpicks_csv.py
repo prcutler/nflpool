4e5b5334e0414144ccb7fa6b9df32bed1511734c
Paul Cutler <paul.r.cutler@gmail.com>
2016-12-07 19:10:21 -0600

    Import player picks from CSV and make into a named tuple (instead of a dictionary)

--- import_playerpicks_csv.py
+++ import_playerpicks_csv.py
@@ -1,16 +1,13 @@
 import csv
+import collections
+from collections import namedtuple
 
-with open('json/2016_playerpicks.csv', 'r') as playerpicks:
-    # fieldnames = [timestamp,firstname,lastname,email,afc_east_first,afc_east_second,afc_east_last,
-#                  afc_north_first,afc_north_second,afc_north_last,afc_south_first,afc_south_second,
-#                  afc_south_last,afc_west_first,afc_west_second,afc_west_last,nfc_east_first,
-#                  nfc_east_second,nfc_east_last,nfc_north_first,nfc_north_second,nfc_north_last,
-#                  nfc_south_first,nfc_south_second,nfc_south_last,nfc_west_first,nfc_west_second,
-#                  nfc_west_last,afc_wildcard1,afc_wildcard2,nfc_wildcard1,nfc_wildcard2,afc_rushing_leader,
-#                  afc_passing_leader,afc_receiving_leader,afc_sacks_leader,afc_int_leader,afc_pf,
-#                  nfc_rushing_leader,nfc_passing_leader,nfc_receiving_leader,nfc_sacks_leader,
-#                  nfc_int_leader,nfc_pf,tiebreaker]
-    reader = csv.DictReader(playerpicks)
-    for row in playerpicks:
-        print(row)
+nfl_pool_players = []
+player_picks = ()
 
+with open('json/2016_playerpicks.csv') as infile:
+    reader = csv.DictReader(infile)
+    picks = collections.namedtuple('picks', reader.fieldnames)
+    player_picks = [picks(**row) for row in reader]
+
+print(player_picks[0])
