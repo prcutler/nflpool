7a8dcb61950d8b15b46c5defcf49f93b076513b8
Paul Cutler <paul.r.cutler@gmail.com>
2017-03-04 17:06:32 -0600

    Add function to add player_picks CSV to database.  Code is not working - too many values to unpack, expects 45, need to re-count.

--- setup/import_playerpicks_csv.py
+++ setup/import_playerpicks_csv.py
@@ -38,14 +38,14 @@ picks = collections.namedtuple('picks',
 
 #with open('json/2016_playerpicks.csv', 'r') as playerpicks:
     # fieldnames = [timestamp,firstname,lastname,email,afc_east_first,afc_east_second,afc_east_last,
-    #                  afc_north_first,afc_north_second,afc_north_last,afc_south_first,afc_south_second,
-    #                  afc_south_last,afc_west_first,afc_west_second,afc_west_last,nfc_east_first,
-    #                  nfc_east_second,nfc_east_last,nfc_north_first,nfc_north_second,nfc_north_last,
-    #                  nfc_south_first,nfc_south_second,nfc_south_last,nfc_west_first,nfc_west_second,
-    #                  nfc_west_last,afc_wildcard1,afc_wildcard2,nfc_wildcard1,nfc_wildcard2,afc_rushing_leader,
-    #                  afc_passing_leader,afc_receiving_leader,afc_sacks_leader,afc_int_leader,afc_pf,
-    #                  nfc_rushing_leader,nfc_passing_leader,nfc_receiving_leader,nfc_sacks_leader,
-    #                  nfc_int_leader,nfc_pf,tiebreaker]
+afc_north_first, afc_north_second, afc_north_last, afc_south_first, afc_south_second,
+afc_south_last, afc_west_first, afc_west_second, afc_west_last, nfc_east_first,
+nfc_east_second, nfc_east_last, nfc_north_first, nfc_north_second, nfc_north_last,
+nfc_south_first, nfc_south_second, nfc_south_last, nfc_west_first, nfc_west_second,
+nfc_west_last, afc_wildcard1, afc_wildcard2, nfc_wildcard1, nfc_wildcard2, afc_rushing_leader,
+afc_passing_leader, afc_receiving_leader, afc_sacks_leader, afc_int_leader, afc_pf,
+nfc_rushing_leader, nfc_passing_leader, nfc_receiving_leader, nfc_sacks_leader,
+nfc_int_leader, nfc_pf, tiebreaker]
 
 
 #    reader = csv.DictReader(playerpicks)
