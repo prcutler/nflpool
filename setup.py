f4dac2aa0cd924e2c5c36307bc776140b0dbefa9
Paul Cutler <paul.r.cutler@gmail.com>
2017-07-10 11:18:35 -0500

    Create new branch due to cookiecutter setup nesting one level too deep. Pyramid working in this new branch.

--- setup.py
+++ setup.py
@@ -10,40 +10,42 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
 
 requires = [
     'pyramid',
-    'pyramid_jinja2',
+    'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'waitress',
-    ]
+]
 
 tests_require = [
     'WebTest >= 1.3.1',  # py3 compat
-    'pytest',  # includes virtualenv
+    'pytest',
     'pytest-cov',
-    ]
+]
 
-setup(name='nflpool',
-      version='0.0',
-      description='nflpool',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Pyramid",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-      ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web pyramid pylons',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      extras_require={
-          'testing': tests_require,
-      },
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = nflpool:main
-      """,
-      )
+setup(
+    name='nflpool',
+    version='0.0',
+    description='nflpool',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = nflpool:main',
+        ],
+    },
+)
