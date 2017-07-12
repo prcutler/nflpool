dcdedca1651728d453d06241055a2808acdd6cd6
Paul Cutler <paul.r.cutler@gmail.com>
2017-07-12 11:40:42 -0500

    Fix cache-busting thanks to Michael Kennedy and office hours of Talk Python training.

--- nflpool/controllers/home_controller.py
+++ nflpool/controllers/home_controller.py
@@ -1,15 +1,16 @@
 import pyramid_handlers
+import nflpool.static_cache
 
-
-class HomeController:
+class ControllerBase:
     def __init__(self, request):
         self.request = request
+        self.build_cache_id = nflpool.static_cache.build_cache_id
 
+class HomeController(ControllerBase):
     @pyramid_handlers.action(renderer='templates/home/index.pt')
     def index(self):
         return {
             'value': 'HOME',
-
         }
 
     @pyramid_handlers.action(renderer='templates/home/about.pt')
