e82ced2385b87d201d8ae92df5c9198ef148a709
Paul Cutler <paul.r.cutler@gmail.com>
2017-07-12 13:46:19 -0500

    Add suppressor.py with suppress decorator to secure Pyramid

--- nflpool/controllers/base_controller.py
+++ nflpool/controllers/base_controller.py
@@ -1,7 +1,14 @@
 import nflpool.static_cache as static_cache
+from nflpool.infrastructure.supressor import suppress
 
 
 class BaseController:
     def __init__(self, request):
         self.request = request
         self.build_cache_id = static_cache.build_cache_id
+
+    @suppress
+    def dont_expose_as_web_action_base(self):
+        print("Called dont_expose_as_web_action, what happened?")
+
+
