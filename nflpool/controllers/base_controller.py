bee9377171dc77c655ed09b34d916caa67177cdc
Paul Cutler <paul.r.cutler@gmail.com>
2017-07-12 21:35:14 -0500

    Add shared layout and refactor index.pt to use the shared layout template.  Change default of Pyramid app to en-us

--- nflpool/controllers/base_controller.py
+++ nflpool/controllers/base_controller.py
@@ -1,11 +1,16 @@
 import nflpool.infrastructure.static_cache as static_cache
 from nflpool.infrastructure.supressor import suppress
+import pyramid.renderers
 
 
 class BaseController:
     def __init__(self, request):
         self.request = request
         self.build_cache_id = static_cache.build_cache_id
+
+        layout_render = pyramid.renderers.get_renderer('nflpool:templates/shared/_layout.pt')
+        impl = layout_render.implementation()
+        self.layout = impl.macros['layout']
 
     @suppress
     def dont_expose_as_web_action_base(self):
