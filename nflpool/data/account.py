e9d57101130d5ce6c917020cf78cef3a16aeec22
Paul Cutler <paul.r.cutler@gmail.com>
2017-07-23 16:17:39 -0500

    Update Account class to include super_user field and password_hash as well as datetime for when the account was created

--- nflpool/data/account.py
+++ nflpool/data/account.py
@@ -1,13 +1,17 @@
 import mongoengine
+import datetime
 import nflpool.data.mongo_setup as mongo_setup
 
 
 class Account(mongoengine.Document):
     first_name = mongoengine.StringField(required=True)
     last_name = mongoengine.StringField(required=True)
-    email = mongoengine.EmailField(required=True)
+    email = mongoengine.EmailField(required=True, unique=True)
     phone = mongo.engine.IntField()
     twitter = mongoengine.StringField()
+    created = mongoengine.DateTimeField(default=datetime.datetime.now)
+    super_user = mongoengine.BooleandField(default=False)
+    password_hash = mongoengine.StringField()
 
     meta = {
         'db_alias': 'core',
