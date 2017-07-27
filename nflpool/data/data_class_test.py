6be39f63457855a466303c6a0836b6507bde2e7e
Paul Cutler <paul.r.cutler@gmail.com>
2017-07-27 14:22:44 -0500

    WIP for trying to get a JSON object into an EmbeddedDocument in MongoDB without any luck so far.

---
+++ nflpool/data/data_class_test.py
@@ -0,0 +1,40 @@
+import mongoengine
+import secret
+from pymongo import MongoClient
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+
+
+class Tiebreaker(mongoengine.EmbeddedDocument):
+    tiebreaker = mongoengine.DynamicField()
+
+
+class Season(mongoengine.Document):
+    year = mongoengine.EmbeddedDocument()
+
+
+class Stats(mongoengine.EmbeddedDocument):
+    tiebreaker = mongoengine.DynamicField()
+    individual = mongoengine.DynamicField()
+    conference = mongoengine.DynamicField()
+    division = mongoengine.DynamicField()
+
+client = MongoClient('localhost', 27017)
+mongoengine.connect('nflpooldb', alias='default')
+db = client.nflpooldb
+collection = db['Season']
+
+year = Season(2016)
+year.save()
+#collection.insert(year)
+
+tiebreaker_data = requests.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular'
+                               '/overall_team_standings.json?teamstats=Kickoff%20Returns.TD,Punt%20Returns.TD',
+                               auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+
+tiebreaker_json = tiebreaker_data.json()
+
+tiebreaker_json = Stats(Tiebreaker())
+tiebreaker_json.save()
+
--- nflpool/data/data_pull.py
+++ nflpool/data/data_pull.py
@@ -4,6 +4,7 @@ from requests.auth import HTTPBasicAuth
 from pymongo import MongoClient
 import json
 import secret
+import mongo_setup
 
 # What year to pull data for
 year = 2016
@@ -16,45 +17,46 @@ data = active_players.json()
 
 client = MongoClient('localhost', 27017)
 db = client.nflpooldb
-collection = db[year]
+collection = db["Season"]["2016"]["17"]
 collection.insert(data)
+#data.insert()
 
 # Pull conference standings and put in the collection of year value above
-conference_standings = requests.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular/'
-                                    'conference_team_standings.json',
-                        auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+#conference_standings = requests.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular/'
+#                                    'conference_team_standings.json',
+#                        auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
 
-conference = conference_standings.json()
+#conference = conference_standings.json()
 
-client = MongoClient('localhost', 27017)
-db = client.nflpooldb
-collection = db[year]
-collection.insert(conference)
-
-# Pull overall Team Standings needed for KrTD and PrTD
-tiebreaker_data = requests.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular'
-                               '/overall_team_standings.json?teamstats=Kickoff%20Returns.TD,Punt%20Returns.TD',
-                        auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
-
-tiebreaker = tiebreaker_data.json()
-
-client = MongoClient('localhost', 27017)
-db = client.nflpooldb
-collection = db[year]
-collection.insert(tiebreaker)
+#client = MongoClient('localhost', 27017)
+#db = client.nflpooldb
+#collection = db["2016"]["week16"]
+#collection.insert(conference)
 
 # Pull overall Team Standings needed for KrTD and PrTD
-tiebreaker_data = requests.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular'
-                               '/overall_team_standings.json?teamstats=Kickoff%20Returns.TD,Punt%20Returns.TD',
-                        auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+#tiebreaker_data = requests.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular'
+#                               '/overall_team_standings.json?teamstats=Kickoff%20Returns.TD,Punt%20Returns.TD',
+#                        auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
 
-tiebreaker = tiebreaker_data.json()
+#tiebreaker = tiebreaker_data.json()
 
-client = MongoClient('localhost', 27017)
-db = client.nflpooldb
-collection = db[year]
-collection.insert(tiebreaker)
+#client = MongoClient('localhost', 27017)
+#db = client.nflpooldb
+#collection = db["2016"]["week16"]
+#collection.insert(tiebreaker)
 
-https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular/cumulative_player_stats.json?position=LB,SS,DT,DE,CB,FS,SS&playerstats=Sacks
+# Pull overall Team Standings needed for KrTD and PrTD
+#tiebreaker_data = requests.get('https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular'
+#                               '/overall_team_standings.json?teamstats=Kickoff%20Returns.TD,Punt%20Returns.TD',
+#                        auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
 
-https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular/cumulative_player_stats.json?position=QB,RB,WR,TE,CB,FS,SS,DE,DT,LB&playerstats=Sacks,
+#tiebreaker = tiebreaker_data.json()
+
+#client = MongoClient('localhost', 27017)
+#db = client.nflpooldb
+#collection = db["2016"]["week16"]
+#collection.insert(tiebreaker)
+
+# https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular/cumulative_player_stats.json?position=LB,SS,DT,DE,CB,FS,SS&playerstats=Sacks
+
+# https://api.mysportsfeeds.com/v1.1/pull/nfl/2016-2017-regular/cumulative_player_stats.json?position=QB,RB,WR,TE,CB,FS,SS,DE,DT,LB&playerstats=Sacks,
