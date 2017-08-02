b0237f44856d89fe3140716ab5f90953bc64becf
Paul Cutler <paul.r.cutler@gmail.com>
2017-08-02 13:12:15 -0500

    Update base_controller to work with account registration.  Fix suppressor from demo

--- nflpool/controllers/base_controller.py
+++ nflpool/controllers/base_controller.py
@@ -1,7 +1,10 @@
 import nflpool.infrastructure.static_cache as static_cache
 from nflpool.infrastructure.supressor import suppress
 import pyramid.renderers
+
 import pyramid.httpexceptions as exc
+import nflpool.infrastructure.cookie_auth as cookie_auth
+from nflpool.services.account_service import AccountService
 
 
 class BaseController:
@@ -17,14 +20,30 @@ class BaseController:
     def is_logged_in(self):
         return False
 
-    @suppress
-    def dont_expose_as_web_action_base(self):
-        print("Called dont_expose_as_web_action, what happened?")
-
     # noinspection PyMethodMayBeStatic
+    @suppress()
     def redirect(self, to_url, permanent=False):
         if permanent:
             raise exc.HTTPMovedPermanently(to_url)
         raise exc.HTTPFound(to_url)
 
+    @property
+    def data_dict(self):
+        data = dict()
+        data.update(self.request.GET)
+        data.update(self.request.POST)
+        data.update(self.request.matchdict)
 
+        return data
+
+    @property
+    def logged_in_user_id(self):
+        return cookie_auth.get_user_id_via_auth_cookie(self.request)
+
+    @property
+    def logged_in_user(self):
+        uid = self.logged_in_user_id
+        if not uid:
+            return None
+
+        AccountService.find_account_by_id(uid)
