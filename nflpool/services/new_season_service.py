186a7d0c18309ac3eda492103a17cb744e69879f
Paul Cutler <paul.r.cutler@gmail.com>
2017-08-05 15:13:39 -0500

    Update new_season service to add OR update the row when adding a year
    This is now working as it should.  This table keeps a record of the
    current season being played and now I need to write the base URL
    functions to include this

--- nflpool/services/new_season_service.py
+++ nflpool/services/new_season_service.py
@@ -4,33 +4,24 @@ from nflpool.data.dbsession import DbSessionFactory
 
 class NewSeasonService:
     @staticmethod
-    def get_install():
+    def new_season():
         return []
 
-    @staticmethod
-    def create_season(season):
+    @classmethod
+    def create_season(cls, season):
         session = DbSessionFactory.create_session()
+        season_row = session.query(SeasonInfo)
 
-        if session.query(SeasonInfo.current_season).count == 0:
-
-            print("There is no season in the database")
+        if season_row.count() == 0:
+            print("New install, adding a season")
             new_season = SeasonInfo()
-
             new_season.current_season = season
             session.add(new_season)
             session.commit()
-
-            return []
-
         else:
-            row_data = session.query(SeasonInfo).filter(SeasonInfo.id == '1')
-            print(row_data)
+            print("Existing season found, updating to new year")
 
-            new_season = SeasonInfo()
-            print("Adding a new season")
+            update_row = session.query(SeasonInfo).filter(SeasonInfo.id == '1').first()
+            update_row.current_season = season
 
-            new_season.current_season = season
-            session.add(new_season)
             session.commit()
-
-            return []
