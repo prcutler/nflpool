775e08e6b70a55209c8fe31691afd904cd75bc1f
Paul Cutler <paul.r.cutler@gmail.com>
2017-08-07 18:11:16 -0500

    Working on the player_picks MVC.  Currently broken.

--- nflpool/services/playerpicks_service.py
+++ nflpool/services/playerpicks_service.py
@@ -2,6 +2,7 @@ from nflpool.data.picks import PlayerPicks
 from nflpool.data.dbsession import DbSessionFactory
 from nflpool.data.account import Account
 from nflpool.data.teaminfo import TeamInfo
+from nflpool.data.picks import PlayerPicks
 
 
 # Need to create a dictionary with team_id : conference / division
@@ -23,3 +24,7 @@ class PlayerPicksService:
             .filter(TeamInfo.division == 'North').order_by(TeamInfo.name).all()
 
         return afc_north_teams
+
+    @classmethod
+    def get_player_picks(cls, afc_east_first: str, afc_east_second: str, afc_north_first: str
+                         afc_north_second: str, user_id: str, date_submitted: int):
--- nflpool/viewmodels/playerpicks_viewmodel.py
+++ nflpool/viewmodels/playerpicks_viewmodel.py
@@ -3,16 +3,20 @@ from nflpool.viewmodels.viewmodelbase import ViewModelBase
 
 class PlayerPicksViewModel(ViewModelBase):
     def __init__(self):
-        self.afc_east_first = None
+        self.afc_east_winner = None
         self.afc_east_second = None
-        self.afc_east_last = None
+        self.afc_north_winner = None
+        self.afc_north_second = None
         self.user_id = None
         self.date = None
+        self.season = None
 
     def from_dict(self, data_dict):
-        self.afc_east_first = data_dict.get('afc_east_first')
+        self.afc_east_winner = data_dict.get('afc_east_first')
         self.afc_east_second = data_dict.get('afc_east_second')
-        self.afc_east_last = data_dict.get('afc_east_last')
+        self.afc_north_winner = data_dict.get('afc_north_first')
+        self.afc_north_second = data_dict.get('afc_north_second')
         self.user_id = data_dict.get('id')
         self.date = data_dict.get('date')
+        self.season = data_dict.get('season')
 
