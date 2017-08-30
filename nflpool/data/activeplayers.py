7b04d6d9fe8ad1c391f6459958e5f37d38956a15
Paul Cutler <paul.r.cutler@gmail.com>
2017-08-29 20:20:14 -0500

    Update primary_key column from id to primary_key

--- nflpool/data/activeplayers.py
+++ nflpool/data/activeplayers.py
@@ -4,7 +4,7 @@ import sqlalchemy
 
 class ActiveNFLPlayers(SqlAlchemyBase):
     __tablename__ = 'ActiveNFLPlayers'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
+    primary_key = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
     season = sqlalchemy.Column(sqlalchemy.Integer)
     team_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('TeamInfo.team_id'))
     firstname = sqlalchemy.Column(sqlalchemy.String)
