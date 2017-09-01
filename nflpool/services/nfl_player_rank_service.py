647b779925637f400df349a3b80320c7b429297a
Paul Cutler <paul.r.cutler@gmail.com>
2017-08-31 21:05:49 -0500

    The interception query is for all NFL, not by conference
    We'll deal with this mess tomorrow

--- nflpool/services/nfl_player_rank_service.py
+++ nflpool/services/nfl_player_rank_service.py
@@ -4,28 +4,56 @@ from nflpool.data.divisioninfo import DivisionInfo
 from nflpool.data.conferenceinfo import ConferenceInfo
 from nflpool.data.picktypes import PickTypes
 from nflpool.data.weekly_nflplayer_stats import WeeklyNFLPlayerStats
+from nflpool.data.activeplayers import ActiveNFLPlayers
+from nflpool.data.teaminfo import TeamInfo
 from itertools import groupby
 
 
 def interception_leaders():
     session = DbSessionFactory.create_session()
 
-    interception_query = session.query(WeeklyNFLPlayerStats.interceptions, WeeklyNFLPlayerStats.player_id).\
-        order_by(WeeklyNFLPlayerStats.interceptions.desc()).limit(20).all()
+    afc_interception_query = session.query(WeeklyNFLPlayerStats.interceptions, WeeklyNFLPlayerStats.player_id).\
+        join(ActiveNFLPlayers.player_id == WeeklyNFLPlayerStats.player_id).\
+        join(ActiveNFLPlayers.team_id == TeamInfo.team_id).\
+        filter(TeamInfo.conference_id == 0).order_by(WeeklyNFLPlayerStats.interceptions.desc()).limit(20).all()
 
-    print(interception_query)
+    # This query pulls all interception leaders for both the AFC and not NFC - not what I need
+#    interception_query = session.query(WeeklyNFLPlayerStats.interceptions, WeeklyNFLPlayerStats.player_id). \
+#        order_by(WeeklyNFLPlayerStats.interceptions.desc()).limit(20).all()
 
-    interception_list = [list(interception_query) for interception_query in interception_query]
-    print(interception_list)
+#    int_list = session.query(ActiveNFLPlayers.player_id, ActiveNFLPlayers.firstname,
+#                             ActiveNFLPlayers.lastname). \
+#        join(TeamInfo, ActiveNFLPlayers.team_id == TeamInfo.team_id) \
+#        .filter(TeamInfo.conference_id == conf_id) \
+#        .filter(ActiveNFLPlayers.position.in_([cb, db, fs, ss, mlb, lb, olb, ilb])) \
+#        .filter(ActiveNFLPlayers.season == SeasonInfo.current_season) \
+#        .order_by(ActiveNFLPlayers.lastname).all()
 
-    sorted_interceptions = sorted(interception_list, reverse=True)
+#    picks_query = session.query(PlayerPicks.pick_type, ConferenceInfo.conference, DivisionInfo.division,
+#                                TeamInfo.name, PlayerPicks.rank,
+#                                ActiveNFLPlayers.firstname, ActiveNFLPlayers.lastname) \
+#        .outerjoin(ConferenceInfo) \
+#        .outerjoin(DivisionInfo) \
+#        .outerjoin(TeamInfo) \
+#        .outerjoin(ActiveNFLPlayers, and_(PlayerPicks.player_id == ActiveNFLPlayers.player_id,
+#                                          PlayerPicks.season == ActiveNFLPlayers.season)). \
+#        filter(PlayerPicks.user_id == user_id,
+#               PlayerPicks.season == season)
+
+    print(afc_interception_query)
+
+    afc_interception_list = [list(afc_interception_query) for afc_interception_query in afc_interception_query]
+    print(afc_interception_list)
+
+    afc_sorted_interceptions = sorted(afc_interception_list, reverse=True)
     rank = 0
-    for _, grp in groupby(sorted_interceptions, key=lambda xs: xs[0]):
+    for _, grp in groupby(afc_sorted_interceptions, key=lambda xs: xs[0]):
         r = rank + 1
         for x in grp:
             x.append(r)
             rank += 1
-            print(sorted_interceptions)
+
+    print(afc_sorted_interceptions)
 
 
 
