00d541f3ee2835a8d835d652eb145eddbe659ee8
Paul Cutler <paul.r.cutler@gmail.com>
2018-04-18 16:19:42 -0500

    Add Slack service
    Add service to send messages to Slack

---
+++ nflpool/services/slack_service.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import mlbpool.data.config as config
+
+
+class SlackService:
+    @staticmethod
+    def send_message(message_string):
+
+    '''Set the webhook_url to the one provided by Slack when you create the webhook
+    at https://my.slack.com/services/new/incoming-webhook/'''
+
+        slack_webhook_url = config.slack_webhook_url
+        slack_data = {'text': message_string}
+
+        response = requests.post(slack_webhook_url, data=json.dumps(slack_data),
+            headers={'Content-Type': 'application/json'})
+        if response.status_code != 200:
+            raise ValueError(
+                'Request to slack returned an error %s, the response is:\n%s'
+                % (response.status_code, response.text))
