936b7b550a6c121ea45577799af77bbae0683f25
Paul Cutler <paul.r.cutler@gmail.com>
2018-05-09 07:35:23 -0500

    Add home and away team columns to SeasonInfo
    Needed to display to the user when the season starts when
    submitting picks

--- nflpool/data/seasoninfo.py
+++ nflpool/data/seasoninfo.py
@@ -7,6 +7,8 @@ class SeasonInfo(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     current_season = sqlalchemy.Column(sqlalchemy.Integer)
     season_start_date = sqlalchemy.Column(sqlalchemy.Integer)
+    home_team = sqlalchemy.Column(sqlalchemy.String)
+    away_team = sqlalchemy.Column(sqlalchemy.String)
 
 
 
