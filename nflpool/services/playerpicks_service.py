913d114bf94167d9b58bf5d675e3a244001b0e86
Paul Cutler <paul.r.cutler@gmail.com>
2018-07-24 10:58:06 -0500

    Add wildcard (Pick type 9) to change process

--- nflpool/services/playerpicks_service.py
+++ nflpool/services/playerpicks_service.py
@@ -825,7 +825,6 @@ class PlayerPicksService:
                     .update({"player_id": nfc_int_pick, "date_submitted": now_time})
 
         # Update Pick Type 9 - Wildcard Teams
-        # TODO - Need to add rank here and to the original pick to make changes correctly
         for pick in session.query(PlayerPicks.team_id).filter(PlayerPicks.user_id == user_id) \
                 .filter(PlayerPicks.season == season) \
                 .filter(PlayerPicks.pick_type == 9) \
@@ -839,7 +838,46 @@ class PlayerPicksService:
                     .filter(PlayerPicks.conf_id == 0) \
                     .update({"team_id": afc_wildcard1_pick, "date_submitted": now_time})
 
-        # TODO Add pick types 2-10
+        for pick in session.query(PlayerPicks.team_id).filter(PlayerPicks.user_id == user_id) \
+                .filter(PlayerPicks.season == season) \
+                .filter(PlayerPicks.pick_type == 9) \
+                .filter(PlayerPicks.rank == 2) \
+                .filter(PlayerPicks.conf_id == 0).first():
+
+            if pick != int(afc_wildcard2_pick):
+                session.query(PlayerPicks).filter(PlayerPicks.user_id == user_id) \
+                    .filter(PlayerPicks.pick_type == 9) \
+                    .filter(PlayerPicks.rank == 2) \
+                    .filter(PlayerPicks.conf_id == 0) \
+                    .update({"team_id": afc_wildcard2_pick, "date_submitted": now_time})
+
+        for pick in session.query(PlayerPicks.team_id).filter(PlayerPicks.user_id == user_id) \
+                .filter(PlayerPicks.season == season) \
+                .filter(PlayerPicks.pick_type == 9) \
+                .filter(PlayerPicks.rank == 1) \
+                .filter(PlayerPicks.conf_id == 1).first():
+
+            if pick != int(nfc_wildcard1_pick):
+                session.query(PlayerPicks).filter(PlayerPicks.user_id == user_id) \
+                    .filter(PlayerPicks.pick_type == 9) \
+                    .filter(PlayerPicks.rank == 1) \
+                    .filter(PlayerPicks.conf_id == 1) \
+                    .update({"team_id": nfc_wildcard1_pick, "date_submitted": now_time})
+
+        for pick in session.query(PlayerPicks.team_id).filter(PlayerPicks.user_id == user_id) \
+                .filter(PlayerPicks.season == season) \
+                .filter(PlayerPicks.pick_type == 9) \
+                .filter(PlayerPicks.rank == 2) \
+                .filter(PlayerPicks.conf_id == 1).first():
+
+            if pick != int(nfc_wildcard2_pick):
+                session.query(PlayerPicks).filter(PlayerPicks.user_id == user_id) \
+                    .filter(PlayerPicks.pick_type == 9) \
+                    .filter(PlayerPicks.rank == 2) \
+                    .filter(PlayerPicks.conf_id == 1) \
+                    .update({"team_id": nfc_wildcard2_pick, "date_submitted": now_time})
+
+        # TODO Add pick type 10
 
 
 class DisplayPlayerPicks:
