19da6c14a5063d3d0361b9b887fd0e4ed8d7a83d
Paul Cutler <paul.r.cutler@gmail.com>
2018-07-29 08:14:28 -0500

    Update SeasonInfo database table info
    Add columns for the first game star time, home and away teams for the
    first NFL game played of the season

--- nflpool/data/seasoninfo.py
+++ nflpool/data/seasoninfo.py
@@ -7,6 +7,6 @@ class SeasonInfo(SqlAlchemyBase):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     current_season = sqlalchemy.Column(sqlalchemy.Integer)
     season_start_date = sqlalchemy.Column(sqlalchemy.Integer)
-
-
-
+    season_start_time = sqlalchemy.Column(sqlalchemy.String)
+    home_team = sqlalchemy.Column(sqlalchemy.String)
+    away_team = sqlalchemy.Column(sqlalchemy.String)
