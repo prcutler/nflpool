b9dd0c4412ce5db861d094dc68ff130d20635ec2
Paul Cutler <paul.r.cutler@gmail.com>
2018-07-29 09:46:52 -0500

    Update Season service to pull updated information
    Pull data from MSF for the  columns for the first game start time, home
    and away teams for the  first NFL game played of the season

--- nflpool/services/new_season_service.py
+++ nflpool/services/new_season_service.py
@@ -1,5 +1,9 @@
 from nflpool.data.seasoninfo import SeasonInfo
 from nflpool.data.dbsession import DbSessionFactory
+import requests
+import pendulum
+import nflpool.data.secret as secret
+from requests.auth import HTTPBasicAuth
 
 
 class NewSeasonService:
@@ -11,22 +15,63 @@ class NewSeasonService:
     in the database.  This is used to for the MySportsFeeds API to get the correct year of stats needed.'''
     # TODO Add logging
     @classmethod
-    def create_season(cls, season, season_start_date):
+    def create_season(cls, season):
         session = DbSessionFactory.create_session()
         season_row = session.query(SeasonInfo)
 
         if season_row.count() == 0:
             print("New install, adding a season")
-            new_season = SeasonInfo()
-            new_season.current_season = season
-            new_season.season_start_date = season_start_date
+
+            response = requests.get('https://api.mysportsfeeds.com/v1.2/pull/nfl/' + str(season) +
+                                    '-regular/full_game_schedule.json',
+                                    auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+
+            gameday_json = response.json()
+            gameday_data = gameday_json["fullgameschedule"]["gameentry"][0]
+
+            first_game_date = gameday_data["date"]
+            first_game_time = gameday_data["time"]
+            away_team = gameday_data["awayTeam"]["Name"]
+            home_team = gameday_data["homeTeam"]["Name"]
+
+            first_game = first_game_date + "T" + first_game_time[:-2]
+            first_game_calc = pendulum.from_format(first_game, '%Y-%m-%dT%H:%M')
+
+            if 1 >= first_game_calc.hour <= 10:
+                first_game = first_game_calc.add(hours=12)
+
+            new_season = SeasonInfo(eason_start_date=first_game, season_start_time=first_game_time,
+                                    home_team=home_team, away_team=away_team, current_season=season)
+
             session.add(new_season)
             session.commit()
+
         else:
             print("Existing season found, updating to new year")
 
+            response = requests.get('https://api.mysportsfeeds.com/v1.2/pull/nfl/' + str(season) +
+                                    '-regular/full_game_schedule.json',
+                                    auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+
+            gameday_json = response.json()
+            gameday_data = gameday_json["fullgameschedule"]["gameentry"][0]
+
+            first_game_date = gameday_data["date"]
+            first_game_time = gameday_data["time"]
+            away_team = gameday_data["awayTeam"]["Name"]
+            home_team = gameday_data["homeTeam"]["Name"]
+
+            first_game = first_game_date + "T" + first_game_time[:-2]
+            first_game_calc = pendulum.from_format(first_game, '%Y-%m-%dT%H:%M')
+
+            if 1 >= first_game_calc.hour <= 10:
+                first_game = first_game_calc.add(hours=12)
+
             update_row = session.query(SeasonInfo).filter(SeasonInfo.id == '1').first()
             update_row.current_season = season
-            update_row.season_start_date = season_start_date
+            update_row.season_start_date = first_game
+            update_row.season_start_time = first_game_time
+            update_row.away_team = away_team
+            update_row.home_team = home_team
 
             session.commit()
