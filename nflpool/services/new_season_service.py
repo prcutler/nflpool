db298454953e3dfd3117d63a42083501fad5ea1a
Paul Cutler <paul.r.cutler@gmail.com>
2018-09-07 18:36:30 -0500

    Update the New Season service to use MySportsFeeds API v2

--- nflpool/services/new_season_service.py
+++ nflpool/services/new_season_service.py
@@ -22,26 +22,30 @@ class NewSeasonService:
         if season_row.count() == 0:
             print("New install, adding a season")
 
-            response = requests.get('https://api.mysportsfeeds.com/v1.2/pull/nfl/' + str(season) +
-                                    '-regular/full_game_schedule.json',
-                                    auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+            response = requests.get('https://api.mysportsfeeds.com/v2/pull/nfl/' + str(season) +
+                                    '-regular/games.json',
+                                    auth=HTTPBasicAuth(secret.msf_api, secret.msf_v2pw))
 
             gameday_json = response.json()
-            gameday_data = gameday_json["fullgameschedule"]["gameentry"][0]
+            gameday_data = gameday_json["games"][0]
 
-            first_game_date = gameday_data["date"]
-            first_game_time = gameday_data["time"]
-            away_team = gameday_data["awayTeam"]["Name"]
-            home_team = gameday_data["homeTeam"]["Name"]
+            # first_game_date = gameday_data["date"]
+            # first_game_time = gameday_data["time"]
 
-            first_game = first_game_date + "T" + "0" + first_game_time[:-2]
-            first_game_calc = pendulum.parse(first_game)
+            # TODO Refactor the first game date and time in the database and change ID in the template
 
-            if first_game_calc.hour <= 11:
-                first_game = first_game_calc.add(hours=12)
-                first_game_instance = pendulum.instance(first_game)
+            first_game_date = gameday_data["startTime"]
+            home_team = gameday_data["homeTeam"]["id"]
+            away_team = gameday_data["awayTeam"]["id"]
 
-            new_season = SeasonInfo(season_start_date=first_game_instance, season_start_time=first_game_time,
+            # first_game = first_game_date + "T" + "0" + first_game_time[:-2]
+            first_game_calc = pendulum.parse(first_game_date)
+
+            # if first_game_calc.hour <= 11:
+            #    first_game = first_game_calc.add(hours=12)
+            #    first_game_instance = pendulum.instance(first_game)
+
+            new_season = SeasonInfo(season_start_date=first_game_instance,
                                     home_team=home_team, away_team=away_team, current_season=season)
 
             session.add(new_season)
@@ -50,28 +54,26 @@ class NewSeasonService:
         else:
             print("Existing season found, updating to new year")
 
-            response = requests.get('https://api.mysportsfeeds.com/v1.2/pull/nfl/' + str(season) +
-                                    '-regular/full_game_schedule.json',
-                                    auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+            response = requests.get('https://api.mysportsfeeds.com/v2/pull/nfl/' + str(season) +
+                                    '-regular/games.json',
+                                    auth=HTTPBasicAuth(secret.msf_api, secret.msf_v2pw))
 
             gameday_json = response.json()
-            gameday_data = gameday_json["fullgameschedule"]["gameentry"][0]
+            gameday_data = gameday_json["games"][0]
 
-            first_game_date = gameday_data["date"]
-            first_game_time = gameday_data["time"]
-            away_team = gameday_data["awayTeam"]["Name"]
-            home_team = gameday_data["homeTeam"]["Name"]
+            first_game_date = gameday_data["startTime"]
+            home_team = gameday_data["homeTeam"]["id"]
+            away_team = gameday_data["awayTeam"]["id"]
 
-            first_game = first_game_date + "T" + "0" + first_game_time[:-2]
-            first_game_calc = pendulum.parse(first_game)
+            # first_game = first_game_date + "T" + "0" + first_game_time[:-2]
+            first_game_calc = pendulum.parse(first_game_date)
 
-            if first_game_calc.hour <= 11:
-                first_game = first_game_calc.add(hours=12)
+            # if first_game_calc.hour <= 11:
+            #    first_game = first_game_calc.add(hours=12)
 
             update_row = session.query(SeasonInfo).filter(SeasonInfo.id == '1').first()
             update_row.current_season = season
-            update_row.season_start_date = pendulum.instance(first_game)
-            update_row.season_start_time = first_game_time
+            update_row.season_start_date = pendulum.instance(first_game_calc)
             update_row.away_team = away_team
             update_row.home_team = home_team
 
